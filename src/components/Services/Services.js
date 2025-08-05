2fbd6176a2e4b47a6cc0b855a56f0d080d429a77
Syed Hassan Ullah <syedhassanullah.0900@gmail.com>
2025-08-05 11:33:30 -0700

    commit

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -8,15 +8,21 @@ function Services() {
                 <h1 className='mb-5 mt-5 pt-5'>Our Services</h1>
             </div>
             <div className='row p-0 m-0'>
-                <div className='col-md-4 p-0 '>
+                <div className='col-md-4 p-0'>
                     <div className='ser-lft-sid'>
-                        <h1>Hello </h1>
+                        <ul>
+                            <li><>hello</></li>
+                            <li><>hello</></li>
+                            <li><>hello</></li>
+                            <li><>hello</></li>
+                            <li><>hello</></li>
+                        </ul>
                     </div>
                 </div>
                 <div className='col-md-8 p-0'>
-                    <div className='ser-rit-sid'>
-                        <div className='container-fluid section-padding'>
-                            <div className='row mt-5 pt-3'>
+                    <div className='ser-rit-sid '>
+                        <div className='container section-padding'>
+                            <div className='row mt-5 pt-3 '>
                                 <h1>Hello Wolrd</h1>
                                 <h1>Hello Wolrd</h1>
                                 <h1>Hello Wolrd</h1>
